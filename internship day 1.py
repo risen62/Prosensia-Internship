1515d50ede1203c047e27300ac6fadcd251a3a49
risen62 <samiullahpanni@gmail.com>
2025-06-24 13:27:32 +0500

    Add files via upload

---
+++ internship day 1.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number  : "))
+number2 = float(input("Enter a decimal number : "))
+name = input("Enter your name : ")
+
+print(f"The number is  : {number}")
+print(f"The decimal number is  : {number2}")
+print(f"Your name is : {name}")
