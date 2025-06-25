db2e5474ea021c0d62e8ae4bcd85b1f0c3c83324
risen62 <samiullahpanni@gmail.com>
2025-06-25 14:13:32 +0500

    Add files via upload

---
+++ internship day 2 task 1.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print(f"Addition : {num1 + num2}")
+print(f"Subtraction: {num1 - num2}")
+print(f"Multiplication : {num1 * num2}")
+if num2 == 0:
+    print ("Division by zero is not allowed")
+else:
+    print(f"Division :{num1/num2}")
