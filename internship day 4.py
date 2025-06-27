cecb3e3b89f9c2673706ca098042bca5c56286f0
risen62 <samiullahpanni@gmail.com>
2025-06-27 12:01:48 +0500

    Add files via upload

---
+++ internship day 4.py
@@ -0,0 +1,17 @@
+# Store immutable student IDs using a tuple
+student_IDs = ("CS023", "CS001", "CS042", "CS077", "CS056")
+
+# Create a set of unique course names
+courses = {"Python", "AI", "ML", "Data Science", "Computer Science"}
+
+# Add a new course to the set
+courses.add("Cyber Security")
+
+# Remove an existing course from the set
+courses.remove("AI")
+
+# Print the updated set of courses
+print(courses)
+
+# Print the tuple of student IDs
+print(student_IDs)
